2513c6425f62b9ac4dd2ed4b3cb52d959a3a9dce
pseudonymhq <pseudonymhq@gmail.com>
2025-10-04 14:35:44 +0800

    Update tht4u.js

--- tht4u.js
+++ tht4u.js
@@ -1,16 +1,11 @@
-const FIXED_DURATION = 60;  
-    const THOUGHT_VERSION = "9:16:23:03.";  
+const FIXED_DURATION = 3;  
+    const THOUGHT_VERSION = "10:03:14:35.";  
 
     const THOUGHT_TEXT = {
       paragraphs: [
-       "stop feeling bad for doing what you had to do in a place that you tried to do everything in. stop feeling bad for having to walk away from a situation, from a person, from a place, from a thing that you gave so much of yourself to, and nothing was ever given back to you.",
-       "you have to stop feeling bad for doing the best thing for you, for doing the thing that's gonna serve you, for doing the thing that seems hard right now but is actually the best possible choice for your life.",
-       "sometimes we feel bad because we think that if we keep trying, or keep pushing, or keep working, that one day they'll finally see what we offer, see who we are. but in the process, you end up breaking your own heart trying to force people into places they clearly don't want to be.",
-       "stop feeling bad for doing what's right for you, because at the end of the day, the only person who's guaranteed to care about you is you. if you keep trying to force yourself to be smaller, little, or less than what you deserve just so somebody can stick around, you will continue to hate the person you've become—because it's not you.",
-       "it's a diminished version of you that you kept holding onto, because you thought it was all you wanted or could ever have. and you can't keep hurting yourself just to get other people to like you.",
-        
+       "dea on top",
       ],
-      author: "ry",
+      author: "mr. crush",
       link: "https://www.facebook.com/share/16zPsE3HH1/"
     };
 
@@ -102,3 +97,4 @@ const FIXED_DURATION = 60;
     }
 
     checkIfDestroyed();
+
